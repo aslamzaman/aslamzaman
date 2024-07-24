a2b4e600bc6a3efc88e563b64500e6bfbeeb7f20
aslam <aslamcmes@gmail.com>
2024-07-24 14:57:24 +0600

    increment added

---
+++ app/increment/layout.js
@@ -0,0 +1,13 @@
+import Layout from "@/components/Layout";
+    
+    
+export const metadata = {
+  title: 'Increment',
+  description: 'Apps created by Aslam Zaman',
+}
+
+
+export default function IncrementLayout({ children }) {
+  return <Layout>{children}</Layout> 
+}
+
---
+++ app/increment/page.js
@@ -0,0 +1,144 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { TextDt, TextBn, BtnSubmit, DropdownEn } from "@/components/Form";
+import { jsPDF } from "jspdf";
+import { fetchData } from "@/lib/utils/FetchData";
+import { DateBangla } from "@/lib/DateBangla";
+
+import { inwordBn } from "@/lib/InwordBn";
+require("@/lib/fonts/SUTOM_MJ-normal");
+require("@/lib/fonts/SUTOM_MJ-bold");
+
+
+
+
+
+const Increment = () => {
+    const [staffData, setStaffData] = useState([]);
+
+    const [waitMsg, setWaitMsg] = useState("");
+    const [refNo, setRefNo] = useState("2024-208");
+    const [dt, setDt] = useState("");
+    const [nm, setNm] = useState("†gv: ûgvqyb Kwei");
+    const [yr, setYr] = useState("2023-2024");
+    const [dt2, setDt2] = useState("");
+    const [taka, setTaka] = useState("40000");
+
+
+    useEffect(() => {
+        const getData = async () => {
+
+            try {
+                const staff = await fetchData(`${process.env.NEXT_PUBLIC_BASE_URL}/api/staff`);
+                const scStaff = staff.filter(staf => staf.placeId._id === "660ae2d4825d0610471e272d");
+                console.log(scStaff)
+                setStaffData(scStaff);
+                setWaitMsg('');
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        getData();
+
+        setDt(localStorage.getItem("incrDt") ? localStorage.getItem("incrDt") : "");
+        setDt2(localStorage.getItem("incrDt2") ? localStorage.getItem("incrDt2") : "");
+        setYr(localStorage.getItem("incrYr") ? localStorage.getItem("incrYr") : "");
+        setRefNo(localStorage.getItem("incrRef") ? localStorage.getItem("incrRef") : "");
+
+    }, [waitMsg])
+
+
+    const createLetter = (e) => {
+        e.preventDefault();
+
+        localStorage.setItem("incrDt", dt);
+        localStorage.setItem("incrDt2", dt2);
+        localStorage.setItem("incrYr", yr);
+        localStorage.setItem("incrRef", refNo);
+
+        const doc = new jsPDF({
+            orientation: 'p',
+            unit: 'mm',
+            format: 'a4',
+            putOnlyUsedFonts: true,
+            floatPrecision: 16 // or "smart", default is 16
+        });
+        const splitName = nm.split(";");
+
+        doc.setFont("SutonnyMJ", "normal");
+        doc.setFontSize(16);
+        doc.text(`¯§viK bs-wmGgBGm/GBPAviwW/${refNo}`, 25, 55, null, null, 'left');
+        doc.text(`${DateBangla(dt)}`, 25, 62, null, null, 'left');
+
+
+        doc.text(`Rbve ${splitName[0]}`, 25, 78, null, null, 'left');
+        doc.text(`${splitName[1]}`, 25, 85, null, null, 'left');
+        doc.text(`wmGgBGm, XvKv-1207`, 25, 92, null, null, 'left');
+
+        doc.setFont("SutonnyMJ", "bold");
+        doc.text(`welq: ${yr} A_© eQ‡ii g~j¨vq‡bi wfwË‡Z ${DateBangla(dt2)} ZvwiL †_‡K Avcbvi  evrmwiK †eZb 5% e„w× KiY cÖm‡½|`, 25, 110, { maxWidth: 159, align: 'left' });
+
+
+        doc.setFont("SutonnyMJ", "normal");
+        doc.text(`Rbve,`, 25, 130, null, null, 'left');
+        doc.text(`${yr} A_© eQ‡ii cvidi‡gÝ g~j¨vq‡bi wfwË‡Z Avcbvi  evrmwiK †eZb 5% e„w×i ci Avcbvi eZ©gvb †eZb ${taka}/-(${inwordBn(taka)}) UvKvq DbœxZ K‡i ${DateBangla(dt2)} ZvwiL †_‡K Kvh©Ki Kiv n‡q‡Q|`, 25, 137, { align: 'justify', maxWidth: 159 });
+
+
+        doc.text(`ab¨ev\`v‡šÍ`, 25, 170, null, null, 'left');
+
+
+        doc.text(`†gv: Igi dviæK nvq\`vi`, 25, 199, null, null, 'left');
+        doc.text(`wbe©vnx cwiPvjK`, 25, 206, null, null, 'left');
+        doc.text(`wmGgBGm`, 25, 213, null, null, 'left');
+
+
+        doc.text(`Abywjwc:`, 25, 245, null, null, 'left');
+        doc.text(`1. GBPAviwW/wcGd`, 25, 252, null, null, 'left');
+
+
+        doc.save(new Date().toISOString() + "-experience-certificate.pdf");;
+        setNm("");
+        setTaka("");
+        setWaitMsg(`Ok: ${Date.now}`);
+    }
+
+
+    return (
+        <>
+            <div className="w-full mb-3 mt-8">
+                <h1 className="w-full text-xl lg:text-3xl font-bold text-center text-blue-700">Staff Increment</h1>
+                <p className="w-full text-center text-blue-300">&nbsp;{waitMsg}&nbsp;</p>
+            </div>
+
+            <div className="px-4 lg:px-6">
+
+                <div className="p-2 overflow-auto">
+                    <form onSubmit={createLetter}>
+                        <div className="w-full grid grid-cols-2 gap-4">
+                            <TextBn Title="Ref. No:" Id="refNo" Change={(e) => { setRefNo(e.target.value) }} Value={refNo} Chr="150" />
+                            <TextDt Title="Date" Id="dt" Change={(e) => { setDt(e.target.value) }} Value={dt} />
+                            <TextBn Title="Year" Id="yr" Change={(e) => { setYr(e.target.value) }} Value={yr} Chr="150" />
+                            <TextDt Title="Effective Date" Id="dt2" Change={(e) => { setDt2(e.target.value) }} Value={dt2} />
+                            <DropdownEn Title="Name" Id="nm" Change={e => setNm(e.target.value)} Value={nm}>
+                                {staffData ? staffData.map(staff => {
+                                    const vl = `${staff.nmBn};${staff.postId.nmBn}`;
+                                    return (
+                                        <option value={vl} key={staff._id}>{staff.nmEn}</option>
+                                    )
+
+                                }) : null}
+                            </DropdownEn>
+                            <TextBn Title="Taka" Id="taka" Change={(e) => { setTaka(e.target.value) }} Value={taka} Chr="150" />
+                        </div>
+                        <BtnSubmit Title="Submit" Class="bg-blue-600 hover:bg-blue-800 text-white" />
+                    </form>
+                </div>
+            </div>
+        </>
+    );
+
+};
+
+export default Increment;
+
+
