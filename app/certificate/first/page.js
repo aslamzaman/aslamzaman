cabdbd9f3284090e3383679a3b4fe90c78b298bf
aslam <aslamcmes@gmail.com>
2024-07-28 14:11:53 +0600

    certificate updated

--- app/certificate/first/page.js
+++ app/certificate/first/page.js
@@ -4,7 +4,7 @@ import { jsPDF } from "jspdf";
 import * as XLSX from 'xlsx';
 require("@/lib/fonts/Lobster-Regular-normal");
 require("@/lib/fonts/OpenSansCondensed-Light-normal");
-import { BtnSubmit, TextDt } from "@/components/Form";
+import { BtnSubmit, DropdownEn, TextDt } from "@/components/Form";
 import { formatedDateSlash, formatedDate } from "@/lib/utils";
 
 
@@ -14,15 +14,11 @@ const Certificate = () => {
     const [dt, setDt] = useState([]);
     const [msg, setMsg] = useState("Seclect an excel file");
     const [periodStart, setPeriodStart] = useState("");
-    const [periodEnd, setPeriodEnd] = useState("");
+    const [quart, setQuart] = useState("");
 
 
     useEffect(() => {
         setDt(formatedDate(new Date()));
-        const pStart = localStorage.getItem('periodStart');
-        const pEnd = localStorage.getItem('periodEnd');
-        setPeriodStart(pStart ? formatedDate(pStart) : formatedDate(new Date()));
-        setPeriodEnd(pEnd ? formatedDate(pEnd) : formatedDate(new Date()));
     }, [])
 
     const fileChangeHandler = async (e) => {
@@ -46,9 +42,6 @@ const Certificate = () => {
 
     const createPdfHanler = (e) => {
         e.preventDefault();
-        localStorage.setItem('periodStart', periodStart);
-        localStorage.setItem('periodEnd', periodEnd);
-
 
         if (stdData.length < 1) {
             setMsg("Please select xlxs file");
@@ -65,13 +58,12 @@ const Certificate = () => {
 
         setMsg("Please wait...");
         //  console.log(stdData);
-        const period = `${formatedDateSlash(periodStart)} to ${formatedDateSlash(periodEnd)}`;
-        console.log(period);
+
         let i = 0;
         const myTimer = setInterval(() => {
-
+           
             if (i > 0) {
-                doc.addImage("/images/certificate/Col_EWG_2023_2024_Yr_1.png", "PNG", 0, 0, 297, 210);
+                doc.addImage(`/images/certificate/Col_EWG_2023_2024_Yr_${quart}.png`, "PNG", 0, 0, 297, 210);
 
                 doc.setFont("Lobster-Regular", "normal");
                 doc.setFontSize(24);
@@ -125,6 +117,12 @@ const Certificate = () => {
                             <div className="mt-5">
                                 <input type="file" onChange={fileChangeHandler} className="w-full px-4 py-1.5 text-gray-600 ring-1 focus:ring-4 ring-blue-300 outline-none rounded duration-300 cursor-pointer" accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" />
                             </div>
+                            <DropdownEn  Title="Qurter" Id="quart" Change={(e) => { setQuart(e.target.value) }} Value={quart}>
+                                <option value="1">Qurter-1</option>
+                                <option value="2">Qurter-2</option>
+                                <option value="3">Qurter-3</option>
+                                <option value="4">Qurter-4</option>
+                            </DropdownEn>
                             <div>
                                 <TextDt Title="Date" Id="dt" Change={(e) => { setDt(e.target.value) }} Value={dt} />
                             </div>
