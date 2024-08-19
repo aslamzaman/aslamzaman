0a1c1cfc6730ebde2369457853e94ae6d724fa0c
aslamzaman <37665857+aslamzaman@users.noreply.github.com>
2024-08-19 16:59:16 +0600

    Update Add.js

--- components/code/Add.js
+++ components/code/Add.js
@@ -109,7 +109,7 @@ const Add = (tbl, datas) => {
 
 
     const str = `  
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { TextEn, BtnSubmit } from "@/components/Form";
 import { postDataToAPI } from "@/lib/utils";
 
@@ -117,11 +117,16 @@ import { postDataToAPI } from "@/lib/utils";
 const Add = ({ message }) => {
 ${stateVar}
     const [show, setShow] = useState(false);
+     const modalRef = useRef("");
 
 
     const showAddForm = () => {
         setShow(true);
         resetVariables();
+        setTimeout(() => {
+            modalRef.current.classList.add('mt-16');
+            modalRef.current.classList.remove('-mt-64');
+        }, 50);
     }
 
 
@@ -162,9 +167,9 @@ ${getValue}
     return (
         <>
             {show && (
-                <div className="fixed inset-0 py-16 bg-black bg-opacity-30 backdrop-blur-sm z-10 overflow-auto">
-                    <div className="w-11/12 md:w-1/2 mx-auto mb-10 bg-white border-2 border-gray-300 rounded-md shadow-md duration-300">
-                        <div className="px-6 md:px-6 py-2 flex justify-between items-center border-b border-gray-300">
+                <div className="fixed inset-0 p-4 bg-black bg-opacity-30 backdrop-blur-sm z-10 overflow-y-scroll">
+                    <div ref={modalRef} className="w-full sm:w-11/12 md:w-9/12 lg:w-7/12 xl:w-1/2 -mt-64 mx-auto mb-20 bg-white border-2 border-gray-300 rounded-md shadow-md duration-500">
+                        <div className="px-4 md:px-6 py-4 flex justify-between items-center border-b border-gray-300 rounded-t-md">
                             <h1 className="text-xl font-bold text-blue-600">Add New Data</h1>
                             <button onClick={closeAddForm} className="w-8 h-8 p-0.5 bg-gray-50 hover:bg-gray-300 rounded-md transition duration-500">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-full h-full stroke-black">
@@ -172,16 +177,20 @@ ${getValue}
                                 </svg>
                             </button>
                         </div>
-                        <div className="px-6 pb-6 text-black">
-                            <form onSubmit={saveHandler}>
-                                <div className="grid grid-cols-1 gap-4 my-4">
+                        <div className="p-4 pb-6 border-0 text-black">
+                            <div className="w-full overflow-auto">
+                                <div className="p-4">
+                                    <form onSubmit={saveHandler}>
+                                        <div className="grid grid-cols-1 gap-4 my-4">
 ${dd}                                    
+                                        </div>
+                                        <div className="w-full flex justify-start">
+                                            <input type="button" onClick={closeAddForm} value="Close" className="bg-pink-600 hover:bg-pink-800 text-white text-center mt-3 mx-0.5 px-4 py-2 font-semibold rounded-md focus:ring-1 ring-blue-200 ring-offset-2 duration-300 cursor-pointer" />
+                                            <BtnSubmit Title="Save" Class="bg-blue-600 hover:bg-blue-800 text-white" />
+                                        </div>
+                                    </form>
                                 </div>
-                                <div className="w-full flex justify-start">
-                                    <input type="button" onClick={closeAddForm} value="Close" className="bg-pink-600 hover:bg-pink-800 text-white text-center mt-3 mx-0.5 px-4 py-2 font-semibold rounded-md focus:ring-1 ring-blue-200 ring-offset-2 duration-300 cursor-pointer" />
-                                    <BtnSubmit Title="Save" Class="bg-blue-600 hover:bg-blue-800 text-white" />
-                                </div>
-                            </form>
+                            </div>
                         </div>
                     </div>
                 </div>
